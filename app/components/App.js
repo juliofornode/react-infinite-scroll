b7649e84b4c1603061d6b9ea9639b018b5895207
Julio <juliofornode@gmail.com>
2016-12-21 13:00:41 -0800

    initial app complete

---
+++ app/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import InfiniteScroller from './InfiniteScroller';
+
+const App = () => {
+  return (
+    <div className="my-app">
+      <h1 style={{textAlign: 'center'}}>Example of Infinite Scrolling</h1>
+      <InfiniteScroller />
+    </div>
+  );
+};
+
+export default App;
